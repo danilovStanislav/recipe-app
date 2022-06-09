39acb7ca8defec163a96399282ffce3b9c8910d4
dnvstan <dstanleysupreme@gmail.com>
2022-06-09 23:03:26 +0400

    added loading spinner

---
+++ src/components/LoadingSpin.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="w-full flex justify-center font-black text-2xl">
+		Loading...
+		<div
+			class="w-10 h-10 ml-3 border-4 border-gray-400 rounded-full border-t-orange-500 animate-spin"
+		></div>
+	</div>
+</template>
