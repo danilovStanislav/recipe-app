7dbc62a407e0aa4d07828857c8c124d893892c3a
dnvstan <dstanleysupreme@gmail.com>
2022-06-09 22:00:20 +0400

    added new breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
+		screens: {
+			xs: '350px',
+			...defaultTheme.screens,
+		},
 		extend: {
 			animation: {
-				'onAppear': 'onAppear .2s ease-in'
+				onAppear: 'onAppear .2s ease-in',
 			},
 			keyframes: {
 				onAppear: {
